3ef0c3e1d549d4cd80996b1a2814e7dbb80789eb
juliandg1995 <gomezjulian1995@gmail.com>
2025-11-23 21:05:36 +0000

    Agrego clase Autor.java

--- Java-TP6/src/Ejercicio2/model/Autor.java
+++ Java-TP6/src/Ejercicio2/model/Autor.java
@@ -5,6 +5,34 @@ package Ejercicio2.model;
  *
  * @author Julian Daniel Gómez <https://github.com/juliandg1995>
  */
-public class Autor {
 
+public class Autor{
+
+    private String id;
+    private String nombre;
+    private String nacionalidad;
+
+    public Autor(String id, String nombre, String nacionalidad){
+        this.id = id;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getNacionalidad(){
+        return nacionalidad;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Autor{id='" + id + "', nombre='" + nombre +
+                "', nacionalidad='" + nacionalidad + "'}");
+    }
 }
+
